78944d2e1a7f557932b06b2a2fa69257d416fab4
wuriyanto <wuriyanto48@yahoo.co.id>
2023-01-23 18:56:19 +0700

    aes test: rename function include : with

--- tests/test_aes_ccm.py
+++ tests/test_aes_ccm.py
@@ -8,10 +8,10 @@ class TestAESCcm(unittest.TestCase):
         key = bytes.fromhex('6368616e676520746869732070617373')
         data = 'exampleplaintext'
 
-        result_encrypt = aes.encrypt_aes128_ccm_hex(key, bytes(data, 'utf-8'))
+        result_encrypt = aes.encrypt_with_aes128_ccm_hex(key, bytes(data, 'utf-8'))
         self.assertIsNotNone(result_encrypt)
 
-        result_decrypt = aes.decrypt_aes128_ccm_hex(key, result_encrypt)
+        result_decrypt = aes.decrypt_with_aes128_ccm_hex(key, result_encrypt)
 
         self.assertEqual(bytes(data, 'utf-8'), result_decrypt)
 
@@ -19,10 +19,10 @@ class TestAESCcm(unittest.TestCase):
         key = bytes.fromhex('48656c6c6f75656a656e6777656b67756538232461616d40')
         data = 'exampleplaintext'
 
-        result_encrypt = aes.encrypt_aes192_ccm_hex(key, bytes(data, 'utf-8'))
+        result_encrypt = aes.encrypt_with_aes192_ccm_hex(key, bytes(data, 'utf-8'))
         self.assertIsNotNone(result_encrypt)
 
-        result_decrypt = aes.decrypt_aes192_ccm_hex(key, result_encrypt)
+        result_decrypt = aes.decrypt_with_aes192_ccm_hex(key, result_encrypt)
 
         self.assertEqual(bytes(data, 'utf-8'), result_decrypt)
 
@@ -30,10 +30,10 @@ class TestAESCcm(unittest.TestCase):
         key = bytes.fromhex('48656c6c6f75656a656e6777656b67756538232461616d403534727475746965')
         data = 'exampleplaintext'
 
-        result_encrypt = aes.encrypt_aes256_ccm_hex(key, bytes(data, 'utf-8'))
+        result_encrypt = aes.encrypt_with_aes256_ccm_hex(key, bytes(data, 'utf-8'))
         self.assertIsNotNone(result_encrypt)
 
-        result_decrypt = aes.decrypt_aes256_ccm_hex(key, result_encrypt)
+        result_decrypt = aes.decrypt_with_aes256_ccm_hex(key, result_encrypt)
 
         self.assertEqual(bytes(data, 'utf-8'), result_decrypt)
     
@@ -42,18 +42,18 @@ class TestAESCcm(unittest.TestCase):
         data = 'exampleplaintext'
 
         with self.assertRaises(Exception):
-            result_encrypt = aes.encrypt_aes128_ccm_hex(key, bytes(data, 'utf-8'))
+            result_encrypt = aes.encrypt_with_aes128_ccm_hex(key, bytes(data, 'utf-8'))
     
     def test_encrypt_decrypt_aes192_ccm_throw_error_with_invalid_key(self):
         key = bytes.fromhex('6368616e6765207468697320706173')
         data = 'exampleplaintext'
 
         with self.assertRaises(Exception):
-            result_encrypt = aes.encrypt_aes192_ccm_hex(key, bytes(data, 'utf-8'))
+            result_encrypt = aes.encrypt_with_aes192_ccm_hex(key, bytes(data, 'utf-8'))
     
     def test_encrypt_decrypt_aes256_ccm_throw_error_with_invalid_key(self):
         key = bytes.fromhex('48656c6c6f75656a656e6777656b67756538232461616d40')
         data = 'exampleplaintext'
 
         with self.assertRaises(Exception):
-            result_encrypt = aes.encrypt_aes256_ccm_hex(key, bytes(data, 'utf-8'))
+            result_encrypt = aes.encrypt_with_aes256_ccm_hex(key, bytes(data, 'utf-8'))
--- tests/test_aes_gcm.py
+++ tests/test_aes_gcm.py
@@ -11,10 +11,10 @@ class TestAESGcm(unittest.TestCase):
         key = bytes.fromhex('6368616e676520746869732070617373')
         data = 'exampleplaintext'
 
-        result_encrypt = aes.encrypt_aes128_gcm_hex(key, bytes(data, 'utf-8'))
+        result_encrypt = aes.encrypt_with_aes128_gcm_hex(key, bytes(data, 'utf-8'))
         self.assertIsNotNone(result_encrypt)
 
-        result_decrypt = aes.decrypt_aes128_gcm_hex(key, result_encrypt)
+        result_decrypt = aes.decrypt_with_aes128_gcm_hex(key, result_encrypt)
 
         self.assertEqual(bytes(data, 'utf-8'), result_decrypt)
 
@@ -22,10 +22,10 @@ class TestAESGcm(unittest.TestCase):
         key = bytes.fromhex('48656c6c6f75656a656e6777656b67756538232461616d40')
         data = 'exampleplaintext'
 
-        result_encrypt = aes.encrypt_aes192_gcm_hex(key, bytes(data, 'utf-8'))
+        result_encrypt = aes.encrypt_with_aes192_gcm_hex(key, bytes(data, 'utf-8'))
         self.assertIsNotNone(result_encrypt)
 
-        result_decrypt = aes.decrypt_aes192_gcm_hex(key, result_encrypt)
+        result_decrypt = aes.decrypt_with_aes192_gcm_hex(key, result_encrypt)
 
         self.assertEqual(bytes(data, 'utf-8'), result_decrypt)
 
@@ -33,10 +33,10 @@ class TestAESGcm(unittest.TestCase):
         key = bytes.fromhex('48656c6c6f75656a656e6777656b67756538232461616d403534727475746965')
         data = 'exampleplaintext'
 
-        result_encrypt = aes.encrypt_aes256_gcm_hex(key, bytes(data, 'utf-8'))
+        result_encrypt = aes.encrypt_with_aes256_gcm_hex(key, bytes(data, 'utf-8'))
         self.assertIsNotNone(result_encrypt)
 
-        result_decrypt = aes.decrypt_aes256_gcm_hex(key, result_encrypt)
+        result_decrypt = aes.decrypt_with_aes256_gcm_hex(key, result_encrypt)
 
         self.assertEqual(bytes(data, 'utf-8'), result_decrypt)
     
@@ -45,21 +45,21 @@ class TestAESGcm(unittest.TestCase):
         data = 'exampleplaintext'
 
         with self.assertRaises(Exception):
-            result_encrypt = aes.encrypt_aes128_gcm_hex(key, bytes(data, 'utf-8'))
+            result_encrypt = aes.encrypt_with_aes128_gcm_hex(key, bytes(data, 'utf-8'))
     
     def test_encrypt_decrypt_aes192_gcm_throw_error_with_invalid_key(self):
         key = bytes.fromhex('6368616e6765207468697320706173')
         data = 'exampleplaintext'
 
         with self.assertRaises(Exception):
-            result_encrypt = aes.encrypt_aes192_gcm_hex(key, bytes(data, 'utf-8'))
+            result_encrypt = aes.encrypt_with_aes192_gcm_hex(key, bytes(data, 'utf-8'))
     
     def test_encrypt_decrypt_aes256_gcm_throw_error_with_invalid_key(self):
         key = bytes.fromhex('48656c6c6f75656a656e6777656b67756538232461616d40')
         data = 'exampleplaintext'
 
         with self.assertRaises(Exception):
-            result_encrypt = aes.encrypt_aes256_gcm_hex(key, bytes(data, 'utf-8'))
+            result_encrypt = aes.encrypt_with_aes256_gcm_hex(key, bytes(data, 'utf-8'))
     
     def test_file_with_decrypt_aes256_gcm(self):
         key = bytes.fromhex('6368616e6765207468697320706173736368616e676520746869732070617373')
@@ -68,6 +68,6 @@ class TestAESGcm(unittest.TestCase):
         encrypted_file_path = os.path.join(BASE_DIR, 'testdata/encrypted_file.bin')
 
         with open(encrypted_file_path, 'rb') as input_file:
-            decrypted_file_data = aes.decrypt_aes256_gcm_hex(key, input_file.read().decode())
+            decrypted_file_data = aes.decrypt_with_aes256_gcm_hex(key, input_file.read().decode())
             with open(actual_file_path, 'rb') as actual_file:
                 self.assertEqual(decrypted_file_data, actual_file.read())
